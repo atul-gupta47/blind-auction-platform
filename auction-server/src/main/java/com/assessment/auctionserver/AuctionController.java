e2f555ea7f6c1d3d6e7bfa0978e98f8f461e1581
Atul Gupta <atul.gupta@kpsbremen.de>
2024-01-23 00:57:45 +0100

    Add Javadoc comments to Auction and User Controllers

--- auction-server/src/main/java/com/assessment/auctionserver/AuctionController.java
+++ auction-server/src/main/java/com/assessment/auctionserver/AuctionController.java
@@ -5,6 +5,9 @@ import com.assessment.auctionserver.dtos.BidDto;
 import com.assessment.auctionserver.dtos.ProductDto;
 import org.springframework.web.bind.annotation.*;
 
+/**
+ * AuctionController class is a REST controller that handles incoming requests for creating products, auctions, bids, and ending auctions.
+ */
 @RestController
 @RequestMapping("/auction-server/v1")
 public class AuctionController {
@@ -15,6 +18,12 @@ public class AuctionController {
 		this.auctionService = auctionService;
 	}
 
+	/**
+	 * Creates a new product.
+	 *
+	 * @param productDto the ProductDto object representing the product to be created
+	 * @return the saved ProductDto object
+	 */
 	@PostMapping("/products")
 	public ProductDto createProduct(@RequestBody ProductDto productDto) {
 		ProductDto savedProductDto = auctionService.registerNewProduct(productDto);
@@ -22,6 +31,12 @@ public class AuctionController {
 	}
 
 
+	/**
+	 * Creates an auction with the provided AuctionDto.
+	 *
+	 * @param auctionDto the AuctionDto object representing the auction to be created
+	 * @return the saved AuctionDto object
+	 */
 	@PostMapping("/auctions")
 	public AuctionDto createAuction(@RequestBody AuctionDto auctionDto) {
 
@@ -30,6 +45,12 @@ public class AuctionController {
 		return savedAuctionDto;
 	}
 
+	/**
+	 * Ends the auction with the given auction ID.
+	 *
+	 * @param auctionId    the ID of the auction to end
+	 * @return the user token of the winner, or null if there was no winner
+	 */
 	@PutMapping("/auctions/{auctionId}")
 	public AuctionDto endAuction(@RequestBody AuctionDto auctionDto, @PathVariable Long auctionId) {
 		AuctionDto savedAuctionDto = auctionService.startAuction(auctionDto);
@@ -37,6 +58,12 @@ public class AuctionController {
 		return savedAuctionDto;
 	}
 
+	/**
+	 * Creates a new bid with the provided BidDto.
+	 *
+	 * @param bidDto the BidDto object representing the bid to be created
+	 * @return the saved BidDto object
+	 */
 	@PostMapping("/bids")
 	public BidDto createBid(@RequestBody BidDto bidDto) {
 
--- user-server/src/main/java/com/assessment/userserver/user/UserController.java
+++ user-server/src/main/java/com/assessment/userserver/user/UserController.java
@@ -8,6 +8,9 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 
+/**
+ * The UserController class handles requests related to user operations.
+ */
 @RestController
 @RequestMapping("user-server/v1//users")
 public class UserController {
@@ -19,12 +22,24 @@ public class UserController {
 		this.userService = userService;
 	}
 
+	/**
+	 * Returns a ResponseEntity containing a list of UserDTO objects representing all users.
+	 *
+	 * @return a ResponseEntity containing a list of UserDTO objects
+	 */
 	@GetMapping
 	public ResponseEntity<List<UserDTO>> getAllUsers() {
 		List<UserDTO> users = userService.getAllUsers();
 		return new ResponseEntity<>(users, HttpStatus.OK);
 	}
 
+	/**
+	 * Retrieves a user by their ID.
+	 *
+	 * @param id the ID of the user
+	 * @return a ResponseEntity containing the UserDTO object representing the user if found, or a ResponseEntity
+	 *         with status NOT_FOUND if the user is not found
+	 */
 	@GetMapping("/{id}")
 	public ResponseEntity<UserDTO> getUserById(@PathVariable Long id) {
 		UserDTO user = userService.getUserById(id);
@@ -33,12 +48,25 @@ public class UserController {
 				new ResponseEntity<>(HttpStatus.NOT_FOUND);
 	}
 
+	/**
+	 * Creates a new user.
+	 *
+	 * @param userDTO the UserDTO object representing the user to be created
+	 * @return a ResponseEntity containing the UserDTO object representing the created user and a HTTP status code of 201 (CREATED)
+	 */
 	@PostMapping
 	public ResponseEntity<UserDTO> createUser(@RequestBody UserDTO userDTO) {
 		UserDTO createdUser = userService.createUser(userDTO);
 		return new ResponseEntity<>(createdUser, HttpStatus.CREATED);
 	}
 
+	/**
+	 * Generates a token for a user.
+	 *
+	 * @param id the ID of the user
+	 * @return a ResponseEntity containing the generated token if successfully generated,
+	 *         or a ResponseEntity with status NOT_FOUND if the user is not found
+	 */
 	@PostMapping("/generate-token/{id}")
 	public ResponseEntity<String> generateToken(@PathVariable Long id) {
 		String token = userService.generateTokenForUser(id);
@@ -47,6 +75,13 @@ public class UserController {
 				new ResponseEntity<>(HttpStatus.NOT_FOUND);
 	}
 
+	/**
+	 * Validates the given token for a user.
+	 *
+	 * @param token the token to be validated
+	 * @return a ResponseEntity with status OK if the token is valid, or a ResponseEntity
+	 *         with status UNAUTHORIZED if the token is invalid
+	 */
 	@PostMapping("/validate-token")
 	public ResponseEntity<Void> validateToken(@RequestBody String token) {
 		boolean isValid = userService.validateTokenForUser(token);
